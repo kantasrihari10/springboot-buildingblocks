8ae5b3cf06a46c29ab220a144253ffd6a822451b
kantasrihari10 <65405754+kantasrihari10@users.noreply.github.com>
2021-08-10 00:20:16 -0400

    testing comment

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -10,6 +10,7 @@ public class SpringbootBuildingblocksApplication {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 		System.out.println("hello2");
 		System.out.println("Hi");
+		System.out.println("hari");
 	}
 
 }
