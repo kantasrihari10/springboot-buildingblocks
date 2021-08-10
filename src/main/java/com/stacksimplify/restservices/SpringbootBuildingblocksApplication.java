59da9b09e181aba4c364da9704e6ef4fa333c50f
kantasrihari10 <65405754+kantasrihari10@users.noreply.github.com>
2021-08-10 00:04:05 -0400

    second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -9,6 +9,7 @@ public class SpringbootBuildingblocksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 		System.out.println("hello2");
+		System.out.println("Hi");
 	}
 
 }
